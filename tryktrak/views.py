1d26914de81c0f8b5ddc2f8efeabb3b04ac4752d
Julia Kwiecień <julia.ultramaryna@gmail.com>
2016-12-28 12:37:53 +0100

    Dodano początkowy stan planszy

--- tryktrak/views.py
+++ tryktrak/views.py
@@ -1,4 +1,5 @@
 from flask import render_template, request
+from collections import OrderedDict
 from tryktrak import app
 import random
 
@@ -11,6 +12,22 @@ def rzut_kostka():
         rzuty.append(rzuty[0])
     return rzuty
 
+class Plansza:
+    def __init__(self):
+        self.stan = OrderedDict()
+        for x in range(24):
+            numer = x+1
+            klucz = 'pole_'+str(numer)
+            self.stan[klucz] = []
+        self.stan['pole_1'] = ['bordo', 'bordo']
+        self.stan['pole_6'] = ['bialy', 'bialy', 'bialy', 'bialy', 'bialy']
+        self.stan['pole_8'] = ['bialy', 'bialy', 'bialy']
+        self.stan['pole_12'] = ['bordo', 'bordo', 'bordo', 'bordo', 'bordo']
+        self.stan['pole_13'] = ['bialy', 'bialy', 'bialy', 'bialy', 'bialy']
+        self.stan['pole_17'] = ['bordo', 'bordo', 'bordo']
+        self.stan['pole_19'] = ['bordo', 'bordo', 'bordo', 'bordo', 'bordo']
+        self.stan['pole_24'] = ['bialy', 'bialy']
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -18,4 +35,5 @@ def index():
 
 @app.route('/gra')
 def gra():
-    return render_template('gra.html', typ_gry=request.values['typ_gry'], rzuty = rzut_kostka())
+    plansza = Plansza()
+    return render_template('gra.html', typ_gry=request.values['typ_gry'], rzuty = rzut_kostka(), stan_gry = plansza.stan)
